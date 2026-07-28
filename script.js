123d904a207be8b915aa03b672592b5e8bd484ea
adarshseth999 <adarshseth999@gmail.com>
2026-07-28 16:03:22 +0530

    making menu bar responsive on click

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 let menuIcon = document.getElementById('menu-icon');
 let navbar = document.querySelector('.navbar');
+let navlinks = document.querySelectorAll('.navbar a');
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x')
+menuIcon.addEventListener("click" , (event) => {
+     menuIcon.classList.toggle('bx-x')
     navbar.classList.toggle('active');
-}
+})
+
+document.addEventListener("click" , (event) => {
+    if( !navbar.contains(event.target) && !menuIcon.contains(event.target)){
+        navbar.classList.remove('active')
+        menuIcon.classList.remove('bx-x')
+    }
+})
+
+navlinks.forEach((link) => {
+    link.addEventListener("click" , (event) =>{
+         navbar.classList.remove('active')
+        menuIcon.classList.remove('bx-x')
+    })
+})
+
+
+
 
