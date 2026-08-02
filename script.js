cc25ebf4515710aef61e53cbef719e502acc64f9
adarshseth999 <adarshseth999@gmail.com>
2026-08-03 00:08:41 +0530

    Fixed bugs, changed form interactions, addded animations, final touches

--- script.js
+++ script.js
@@ -21,6 +21,63 @@ navlinks.forEach((link) => {
     })
 })
 
+const form = document.getElementById("contactForm");
+const submitBtn = document.getElementById("submitBtn");
+// const successMessage = document.getElementById("successMessage");
+
+form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    submitBtn.disabled = true;
+    submitBtn.textContent = "Sending...";
+    submitBtn.classList.add("disabled");
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const message = document.getElementById("message").value;
+
+    try {
+        const response = await fetch("https://formspree.io/f/xzdnydjj", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                phone,
+                message,
+            }),
+        });
+
+        if (response.ok) {
+            // Clear all fields
+            form.reset();
+
+            // Show success message
+            // successMessage.style.display = "block";
+
+            // Hide it after 3 seconds
+            // setTimeout(() => {
+                // successMessage.style.display = "none";
+                
+            // }, 3000);
+            alert("Message sent successfully!");
+            
+        } else {
+            alert("Failed to send message.");
+        }
+    } catch (error) {
+        console.error(error);
+        alert("Something went wrong.");
+    }
+
+    submitBtn.disabled = false;
+    submitBtn.textContent = "Send Message";
+    submitBtn.classList.remove("disabled");
+});
+
 
 
 
